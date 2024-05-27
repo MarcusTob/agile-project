5a7811e5dfabe24b3f5e519eda77f6098a4e617b
AndreasIL1 <Andreas.langedal@gmail.com>
2024-05-27 19:25:56 +0200

    test
    h1 test

---
+++ vizrt-marketplace/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Starterpacks from './Starterpacks'
+
+const Home = () => {
+  return (
+    <div className='home-conatiner'>
+        <div className='starterpacks'>
+            <Starterpacks/>
+        </div>
+        </div>
+  )
+}
+
+export default Home
