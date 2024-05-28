65b45dbd84c228deda9275d28cf1ecd6eba141a8
MarcusTob <112120399+MarcusTob@users.noreply.github.com>
2024-05-28 15:37:07 +0200

    bugfix

--- marketplace_api/obj/Debug/net8.0/marketplace-api.AssemblyInfo.cs
+++ marketplace_api/obj/Debug/net8.0/marketplace-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("marketplace-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a667597f19bfa797157cfef85c33d22f780b4ba0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5d4b29aea55e33ee0457265ce196fa22d79acca")]
 [assembly: System.Reflection.AssemblyProductAttribute("marketplace-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("marketplace-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
