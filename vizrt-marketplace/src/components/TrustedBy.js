e424524b087428871ede30047f582d76f6a24189
AndreasIL1 <Andreas.langedal@gmail.com>
2024-05-28 22:53:19 +0200

    Trusted by

---
+++ vizrt-marketplace/src/components/TrustedBy.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const TrustedBy = () => {
+  return (
+    <div className="bg-white py-10 w-full flex flex-col items-center">
+              <h2 className="text-xl font-semibold mb-6">Trusted By</h2>
+      <div className="flex justify-around w-3/4">
+        <img src="/images/pga-tour.webp" alt="pga tour " className="h-16 w-auto" />
+        <img src="/images/nfl-logo.png" alt="nfl" className="h-16 w-auto" />
+        <img src="/images/moviestar-logo.png" alt="moviestar" className="h-16 w-auto" />
+      </div>
+    </div>
+  );
+};
+
+export default TrustedBy;
--- vizrt-marketplace/src/pages/Home.js
+++ vizrt-marketplace/src/pages/Home.js
@@ -1,23 +1,28 @@
 import React from 'react'
 import Starterpack from '../components/Starterpack'
 import Welcomeoffer from '../components/Welcomeoffer';
-import Carousel from '../components/Carousel';
+import TrustedBy from '../components/TrustedBy';
 import "../index.css";
 import "tailwindcss/tailwind.css"
 
 const Home = () => {
   return (
     <div className="bg-brandBgLight min-h-screen flex flex-col items-center">
-        <div>
+        <div className='mt-10'>
             <Starterpack/>
         </div>
-        <div>
-          <Carousel/>
-          </div>
-        <div>
+      
+      <div></div>
+
+        <div className='my-20'>
           <Welcomeoffer/>
+        </div>
+
+        <div className='w-full bg-white'>
+          <TrustedBy/>
         </div>
     </div>
   )
 }
 export default Home;
+
