6902bb68b1fa1cd587455b87e2faf8f810201a4c
AndreasIL1 <Andreas.langedal@gmail.com>
2024-05-29 00:29:03 +0200

    Finisshed landing page

--- vizrt-marketplace/src/pages/Home.js
+++ vizrt-marketplace/src/pages/Home.js
@@ -1,18 +1,27 @@
 import React from 'react'
-import Starterpack from '../components/Starterpack'
+import Starterpack from '../components/Starterpack';
+import Carousel from '../components/Carousel';
 import Welcomeoffer from '../components/Welcomeoffer';
 import TrustedBy from '../components/TrustedBy';
 import "../index.css";
 import "tailwindcss/tailwind.css"
 
 const Home = () => {
+  let slides = [
+    "/images/Image1-carousel.webp",
+    "/images/Image2-carousel.jpeg",
+    "/images/Image3-carousel.webp",
+    "/images/Image4-carousel.png"
+  ]
   return (
     <div className="bg-brandBgLight min-h-screen flex flex-col items-center">
         <div className='mt-10'>
             <Starterpack/>
         </div>
       
-      <div></div>
+      <div className='w-[60%] m-auto pt-11'>
+        <Carousel slides={slides}/>
+      </div>
 
         <div className='my-20'>
           <Welcomeoffer/>
