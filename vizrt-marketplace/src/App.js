e4bb8927226d666db8e064425a506786905c44c7
tola022 <tola022@student.kristiania.no>
2024-05-28 12:01:20 +0200

    GraphicsListPage + filter options

--- vizrt-marketplace/src/App.js
+++ vizrt-marketplace/src/App.js
@@ -1,13 +1,20 @@
 import react from 'react';
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import './App.css';
+import GraphicsListPage from './pages/GraphicsListPage';
 import Home from './pages/Home';
+
+
 
 function App() {
   return (
-    <div className='app'>
-      <Home/>
-        <h1>hei</h1>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/Home" element={<Home />}></Route>
+          <Route path="/GraphicsListPage" element={<GraphicsListPage />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ vizrt-marketplace/src/components/FilterOptions.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const FilterOptions = ({ filter, setFilter, sortByPrice, setSortByPrice }) => {
+    return (
+        <div className="mb-4">
+            <h2 className="text-xl font-bold mb-4">Filters</h2>
+            <div className="mb-4">
+                <label htmlFor="categoryFilter" className="block text-sm font-medium text-gray-700">Category:</label>
+                <select
+                    id="categoryFilter"
+                    value={filter}
+                    onChange={e => setFilter(e.target.value)}
+                    className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+                >
+                    <option value="">All</option>
+                    <option value="3D Graphics">3D Graphics</option>
+                    <option value="Category 2">Category 2</option>
+                    <option value="Category 3">Category 3</option>
+                </select>
+            </div>
+            <div>
+                <label htmlFor="priceSort" className="block text-sm font-medium text-gray-700">Price:</label>
+                <select
+                    id="priceSort"
+                    value={sortByPrice}
+                    onChange={e => setSortByPrice(e.target.value)}
+                    className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+                >
+                    <option value="">None</option>
+                    <option value="lowToHigh">Low to High</option>
+                    <option value="highToLow">High to Low</option>
+                </select>
+            </div>
+        </div>
+    );
+}
+
+export default FilterOptions;
