84b1a98bac516acc8344f3bde1490dd287d3e305
AndreasIL1 <Andreas.langedal@gmail.com>
2024-05-28 11:07:52 +0200

    CAROUSEL

--- vizrt-marketplace/src/components/Carousel.js
+++ vizrt-marketplace/src/components/Carousel.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.min.css';
+import 'swiper/swiper.min.css';
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 const Carousel = () => {
   return (
-    <div>
-      
+    <div className="w-full">
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
+      >
+        <SwiperSlide>
+          <img src="/images/Image1-carousel.jpeg" alt="Slide 1" className="object-cover w-full h-64"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/images/Image2-carousel.jpeg" alt="Slide 2" className="object-cover w-full h-64"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/images/Image3-carousel.jpeg" alt="Slide 3" className="object-cover w-full h-64"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="/images/Image4-carousel.jpeg" alt="Slide 4" className="object-cover w-full h-64"/>
+        </SwiperSlide>
+        {/* Add more slides as needed */}
+      </Swiper>
     </div>
-  )
-}
+  );
+};
 
-export default Carousel
+export default Carousel;
--- vizrt-marketplace/src/pages/Home.js
+++ vizrt-marketplace/src/pages/Home.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Starterpack from '../components/Starterpack'
+import Carousel from '../components/Carousel'
 
 const Home = () => {
   return (
     <div className='home-conatiner'>
         <div className='starterpack'>
             <Starterpack/>
+            <div className='carousel'>
+              <Carousel/>
+            </div>
         </div>
         </div>
   )
