c8adf15274686f0750522f911bc2ba4327f264dd
AndreasIL1 <Andreas.langedal@gmail.com>
2024-05-29 17:08:13 +0200

    navbar

--- vizrt-marketplace/src/components/NavBar.jsx
+++ vizrt-marketplace/src/components/NavBar.jsx
@@ -1,145 +1,55 @@
-import PropTypes from "prop-types";
-import React from "react";
+import React, { useState } from "react";
+import { FiShoppingCart } from "react-icons/fi";
+import { CgProfile } from "react-icons/cg";
 import '../index.css';
 import 'tailwindcss/tailwind.css';
 
-// Navbar Component
-export const Navbar = ({
-  property1 = "default",
+const Navbar = ({
   logoLogo = "https://c.animaapp.com/2XehKRee/img/logo-2@2x.png",
-  search = "https://c.animaapp.com/2XehKRee/img/search-1.svg",
-  user = "https://c.animaapp.com/2XehKRee/img/user-1.svg",
-  shoppingCart = "https://c.animaapp.com/2XehKRee/img/shopping-cart.svg",
-  img = "https://c.animaapp.com/2XehKRee/img/shopping-cart-1.svg",
 }) => {
-  return (
-    <div className="flex justify-center bg-white relative">
-      <div className="w-[1512px] h-[154px] bg-white relative">
-        <div className="w-[1108px] left-[32px] top-[30px] h-[124px] absolute">
-          <div className="w-[805px] left-0 top-0 h-[124px] absolute">
-            <div className="w-[589px] left-[216px] top-[70px] h-[45px] absolute">
-              <div className="w-[379px] left-[204px] top-0 h-[45px] absolute">
-                <div className="w-[204px] left-0 text-center text-[25px] text-[#000000] h-[45px] font-medium absolute">
-                  Packages
-                </div>
-                <div className="w-[204px] left-[175px] text-center text-[25px] text-[#000000] h-[45px] font-medium absolute">
-                  Tools
-                </div>
-              </div>
-              <div className="w-[204px] left-0 text-center text-[25px] text-[#000000] h-[45px] font-medium absolute">
-                <a href="/graphics">Graphics</a>
-              </div>
-            </div>
-            <a href="/">
-              <Logo className="left-0 top-0" logo={logoLogo} />
-            </a>
-          </div>
-          <div className="border-2 border-solid border-[#000000] w-[840px] flex left-[268px] items-center top-[6px] gap-[12px] pl-[12px] pr-[16px] py-[8px] h-[58px] rounded-[8px] bg-white absolute">
-            <img
-              className="w-[24px] h-[24px] relative"
-              alt="Search"
-              src={property1 === "variant-2" ? "search.svg" : search}
-            />
-            <div className="flex-1 text-[#828282] overflow-hidden relative">
-              Search
-            </div>
-          </div>
-        </div>
-        <div
-          className={`absolute ${property1 === "variant-2" ? "w-[152px]" : "w-[251px]"} ${
-            property1 === "variant-2" ? "left-[1328px]" : "left-[1229px]"
-          } ${property1 === "variant-2" ? "top-[26px]" : "top-[15px]"} ${
-            property1 === "variant-2" ? "h-[65px]" : "h-[129px]"
-          }`}
-        >
-          {property1 === "variant-2" && (
-            <>
-              <img className="absolute w-[55px] h-[59px] top-[6px] left-[97px]" alt="User" src={user} />
-              <img className="absolute w-[53px] h-[47px] top-[12px] left-0" alt="Shopping cart" src={img} />
-              <Badge
-                badgeLabelClassName="!left-[7px] !top-[7px]"
-                badgeLabelSingleDigit="3"
-                className="!h-[27px] !absolute !left-[39px] !w-[27px] !top-0"
-                size="large-single-digit"
-              />
-            </>
-          )}
+  const [searchTerm, setSearchTerm] = useState("");
 
-          {property1 === "default" && (
-            <div className="absolute w-[152px] h-[65px] top-[11px] left-[99px]">
-              <img className="absolute w-[55px] h-[59px] top-[6px] left-[97px]" alt="User" src={user} />
-              <div className="absolute w-[66px] h-[59px] top-0 left-0">
-                <img className="absolute w-[53px] h-[47px] top-[12px] left-0" alt="Shopping cart" src={shoppingCart} />
-                <Badge
-                  badgeLabelClassName="!left-[7px] !top-[7px]"
-                  badgeLabelSingleDigit="3"
-                  className="!h-[27px] !absolute !left-[39px] !w-[27px] !top-0"
-                  size="large-single-digit"
-                />
-              </div>
-            </div>
-          )}
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  return (
+    <div className="flex justify-between items-center bg-white p-4 shadow-md">
+      {/* Logo */}
+      <a href="/" className="flex items-center">
+        <img src={logoLogo} alt="Vizrt Logo" className="w-40 h-auto object-cover" />
+      </a>
+
+      {/* Search Bar and Links */}
+      <div className="flex flex-col items-center w-1/2">
+        <div className="border-2 border-solid border-gray-600 flex items-center w-full gap-2 p-2 rounded-md bg-white">
+          <img
+            className="w-6 h-6"
+            alt="Search"
+            src="https://c.animaapp.com/2XehKRee/img/search-1.svg"
+          />
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={handleSearchChange}
+            placeholder="Search"
+            className="flex-1 text-gray-600 bg-transparent outline-none"
+          />
         </div>
+        <div className="flex space-x-8 mt-2">
+          <a href="/graphics" className="text-black text-xl font-medium">Graphics</a>
+          <a href="/packages" className="text-black text-xl font-medium">Packages</a>
+          <a href="/tools" className="text-black text-xl font-medium">Tools</a>
+        </div>
+      </div>
+
+      {/* Icons */}
+      <div className="flex space-x-6 items-center">
+        <FiShoppingCart className="text-3xl text-black" />
+        <CgProfile className="text-3xl text-black" />
       </div>
     </div>
   );
-};
-
-// Logo Component
-const Logo = ({ className, logoClassName, logo }) => {
-  return (
-    <div className={`relative w-[216px] h-[116px] ${className}`}>
-      <img className={`absolute w-[171px] h-[92px] top-0 left-0 object-cover ${logoClassName}`} alt="Logo" src={logo} />
-    </div>
-  );
-};
-
-// Badge Component
-const Badge = ({ badgeLabelClassName, badgeLabelSingleDigit, className, size }) => {
-  return (
-    <div className={`relative ${className}`}>
-      <div className={`absolute text-white bg-red-600 rounded-full text-center ${size === "large-single-digit" ? "w-[27px] h-[27px]" : ""} ${badgeLabelClassName}`}>
-        {badgeLabelSingleDigit}
-      </div>
-    </div>
-  );
-};
-
-// PropTypes and DefaultProps for Navbar
-Navbar.propTypes = {
-  property1: PropTypes.oneOf(["variant-2", "default"]),
-  logoLogo: PropTypes.string,
-  search: PropTypes.string,
-  user: PropTypes.string,
-  shoppingCart: PropTypes.string,
-  img: PropTypes.string,
-};
-
-// PropTypes and DefaultProps for Logo
-Logo.propTypes = {
-  className: PropTypes.string,
-  logoClassName: PropTypes.string,
-  logo: PropTypes.string,
-};
-
-Logo.defaultProps = {
-  className: "",
-  logoClassName: "",
-  logo: "https://c.animaapp.com/2XehKRee/img/logo-2@2x.png",
-};
-
-// PropTypes and DefaultProps for Badge
-Badge.propTypes = {
-  badgeLabelClassName: PropTypes.string,
-  badgeLabelSingleDigit: PropTypes.string.isRequired,
-  className: PropTypes.string,
-  size: PropTypes.string,
-};
-
-Badge.defaultProps = {
-  badgeLabelClassName: "",
-  className: "",
-  size: "large-single-digit",
 };
 
 export default Navbar;
