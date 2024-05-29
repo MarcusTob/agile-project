c755820d0bddae9a1c54042629f38e21aa019205
MarcusTob <112120399+MarcusTob@users.noreply.github.com>
2024-05-29 20:35:15 +0200

    added packagelistpage, but missing filter option

--- vizrt-marketplace/src/pages/PackagesListPage.js
+++ vizrt-marketplace/src/pages/PackagesListPage.js
@@ -1,13 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import FilterOptions from '../components/FilterOptions';
 import PackageList from '../components/PackageList';
 import PackageService from '../services/PackageService';
 
 const PackageListPage = () => {
     // State to store the list of all products
     const [graphicPackage, setGraphicPackage] = useState([]);
-    // State to store the list of filtered products
-    const [filteredPackages, setFilteredPackages] = useState([]);
 
     // Function to fetch all products from the API
     const getAllPackages = async () => {
@@ -15,8 +12,6 @@ const PackageListPage = () => {
         const response = await PackageService.getAllPackages();
         // Set the fetched products to the products state
         setGraphicPackage(response);
-        // Initialize filteredProducts with all products initially
-        setFilteredPackages(response);
     };
 
     // useEffect to fetch products when the component mounts
@@ -26,15 +21,7 @@ const PackageListPage = () => {
 
     return (
         <div className="flex bg-brandBgLight">
-            <div className="pl-4">
-                {/* Pass products and setFilteredProducts function to FilterOptions */}
-                <FilterOptions
-                    graphicPackages={graphicPackage}
-                    setFilteredPackages={setFilteredPackages}
-                />
-            </div>
-            {/* Pass the filtered products to ProductList for display */}
-            <PackageList graphicPackages={filteredPackages} />
+            <PackageList graphicPackages={graphicPackage} />
         </div>
     );
 };
