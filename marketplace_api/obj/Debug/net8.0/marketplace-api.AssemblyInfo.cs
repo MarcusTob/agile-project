ccab29787689366175c8e7a27f22baf7592a24fb
MarcusTob <112120399+MarcusTob@users.noreply.github.com>
2024-05-29 13:50:01 +0200

    .

--- marketplace_api/obj/Debug/net8.0/marketplace-api.AssemblyInfo.cs
+++ marketplace_api/obj/Debug/net8.0/marketplace-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("marketplace-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+908839afdd4ef607333c51c051a03697eaa16d2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8994c0d1d804155c734c4e2e116c1c053c3607c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("marketplace-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("marketplace-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
