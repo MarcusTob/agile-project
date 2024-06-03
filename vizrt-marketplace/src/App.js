cc0f5c13878f72b4f307e82da58aab338123a261
AndreasIL1 <Andreas.langedal@gmail.com>
2024-06-03 13:40:30 +0200

    Selling/Upload

--- vizrt-marketplace/src/App.js
+++ vizrt-marketplace/src/App.js
@@ -6,6 +6,7 @@ import ProductPage from './pages/ProductPage';
 import ShoppingCart from './pages/ShoppingCart';
 import PackagesListPage from './pages/PackagesListPage';
 import PackagePage from './pages/PackagePage';
+import SellingUploadPage from './pages/SellingUploadPage';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import RegisterUser from './pages/RegisterUser';
 import Login from './pages/Login';
@@ -26,6 +27,7 @@ function App() {
           <Route path="/registeruser" element={<RegisterUser />} />
           <Route path="/login" element={<Login />} />
           <Route path="/tools" element={<Tools/>} />
+          <Route path="/sellingUpload" element={<SellingUploadPage/>} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ vizrt-marketplace/src/pages/SellingUploadPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SellingUpload from '../components/SellingUpload'
+
+const SellingUploadPage = () => {
+  return (
+    <div>
+      <SellingUpload/>
+    </div>
+  )
+}
+
+export default SellingUploadPage
+
