862461390c22fe36da27728a8e4ffbab104f8a47
MarcusTob <112120399+MarcusTob@users.noreply.github.com>
2024-06-03 17:48:32 +0200

    prep for merge

--- marketplace_api/obj/Debug/net8.0/marketplace-api.AssemblyInfo.cs
+++ marketplace_api/obj/Debug/net8.0/marketplace-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("marketplace-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15fb525fbe14f92052092f99fe76d2dfbbb43c50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad06e28723ec2d33be9d4fb7814d8eb75f181817")]
 [assembly: System.Reflection.AssemblyProductAttribute("marketplace-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("marketplace-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
