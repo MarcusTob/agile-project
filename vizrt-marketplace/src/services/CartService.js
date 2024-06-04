8d447bc49904fb69d98884f7d503000c309bd13b
MarcusTob <112120399+MarcusTob@users.noreply.github.com>
2024-06-04 21:22:46 +0200

    fixed

--- vizrt-marketplace/src/services/CartService.js
+++ vizrt-marketplace/src/services/CartService.js
@@ -13,6 +13,7 @@ const CartService = {
     const cart = CartService.getCart(); // Retrieve current cart
     cart.push(item); // Add item to cart
     localStorage.setItem("cart", JSON.stringify(cart)); // Update cart in local storage
+    CartService.notifySubscribers();
   },
   
   // Method to remove an item from the cart by index
@@ -20,12 +21,24 @@ const CartService = {
     const cart = CartService.getCart(); // Retrieve current cart
     cart.splice(index, 1); // Remove item from cart at specified index
     localStorage.setItem("cart", JSON.stringify(cart)); // Update cart in local storage
+    CartService.notifySubscribers();
   },
   
   // Method to update the entire cart with a new array of items
   updateCart: (updatedCart) => {
     localStorage.setItem("cart", JSON.stringify(updatedCart)); // Update cart in local storage
+    CartService.notifySubscribers();
   },
+  subscribe: (callback) => {
+    subscribers.push(callback);
+    return () => {
+      subscribers = subscribers.filter(subscriber => subscriber !== callback);
+    };
+  },
+  notifySubscribers: () => {
+    const cart = CartService.getCart();
+    subscribers.forEach((subscriber) => subscriber(cart));
+  }
 };
 
 export default CartService; // Export the CartService object
