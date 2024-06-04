092790acc98642b1f3c574783b516af361da7c55
AndreasIL1 <Andreas.langedal@gmail.com>
2024-06-04 13:05:07 +0200

    navbar on profile pages

--- vizrt-marketplace/src/pages/SellingUploadPage.js
+++ vizrt-marketplace/src/pages/SellingUploadPage.js
@@ -1,10 +1,15 @@
 import React from "react";
 import SellingUpload from "../components/SellingUpload";
+import MyNavbar from "../components/MyNavbar";
+
 
 const SellingUploadPage = () => {
   return (
     <div>
+    <MyNavbar/>
+    <div>
       <SellingUpload />
+    </div>
     </div>
   );
 };
