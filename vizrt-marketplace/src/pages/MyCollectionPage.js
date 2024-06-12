3eb684d5944e5ccbe1caf5a52ca5f5fd696085c3
Certified Code <pekr014@student.kristiania.no>
2024-06-12 12:24:56 +0200

    Update to shoppingcart

--- vizrt-marketplace/src/pages/MyCollectionPage.js
+++ vizrt-marketplace/src/pages/MyCollectionPage.js
@@ -1,40 +1,27 @@
 import React, { useEffect, useState } from "react";
-import ProductService from "../services/ProductService";
 import CollectionList from "../components/Profile/CollectionList";
 import MyNavbar from "../components/Profile/MyNavbar";
 
-// MyCollectionPage component
 const MyCollectionPage = () => {
-  // State to store all products and filtered products
-  const [products, setProducts] = useState([]);
-  const [filteredProducts, setFilteredProducts] = useState([]);
+  // State to store the collection items
+  const [collection, setCollection] = useState([]);
 
-  // Function to fetch all products
-  const getAllProducts = async () => {
-    // Fetch products from ProductService
-    const response = await ProductService.getAllProducts();
-    // Set the fetched products to the products state
-    setProducts(response);
-    // Initialize filteredProducts with all products initially
-    setFilteredProducts(response);
-  };
-
-    // useEffect to fetch products when the component mounts
-    useEffect(() => {
-        getAllProducts();
-    }, []);
-    
-    return (
-        <div className=" bg-brandBgLight min-h-screen">
-            <MyNavbar />
-            <div className="flex flex-col items-center px-4 py-8" >
-                <div className="w-full max-w screen-xl">
-                    <CollectionList products={products} />
-                </div>
-            </div>
+  // useEffect to fetch collection from local storage when the component mounts
+  useEffect(() => {
+    const storedCollection = JSON.parse(localStorage.getItem('myCollection')) || [];
+    setCollection(storedCollection);
+  }, []);
+  
+  return (
+    <div className="bg-brandBgLight min-h-screen">
+      <MyNavbar />
+      <div className="flex flex-col items-center px-4 py-8">
+        <div className="w-full max-w-screen-xl">
+          <CollectionList products={collection} />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
-
 
 export default MyCollectionPage;
