53b6ada5558d73d30f5aa7fc721a82c124eef297
MarcusTob <112120399+MarcusTob@users.noreply.github.com>
2024-06-13 18:14:15 +0200

    last commit for me

--- vizrt-marketplace/src/services/CartService.js
+++ vizrt-marketplace/src/services/CartService.js
@@ -1,30 +1,31 @@
 let subscribers = [];
 
 const CartService = {
+  //gets cart from localstorage
   getCart: () => {
     const cart = localStorage.getItem("cart");
     return cart ? JSON.parse(cart) : [];
   },
-  
+  //adds item to cart, in local storage
   addToCart: (item) => {
     const cart = CartService.getCart();
     cart.push(item);
     localStorage.setItem("cart", JSON.stringify(cart));
     CartService.notifySubscribers();
   },
-  
+  //removes item from cart
   removeFromCart: (index) => {
     const cart = CartService.getCart();
     cart.splice(index, 1);
     localStorage.setItem("cart", JSON.stringify(cart));
     CartService.notifySubscribers();
   },
-  
+  //updates cart
   updateCart: (updatedCart) => {
     localStorage.setItem("cart", JSON.stringify(updatedCart));
     CartService.notifySubscribers();
   },
-  
+  //moves items from cart, to orders (mycollection)
   placeOrder: () => {
     const cart = CartService.getCart();
     if (cart.length > 0) {
@@ -35,19 +36,19 @@ const CartService = {
       CartService.notifySubscribers();
     }
   },
-  
+  //gets items from orders (mycollection)
   getOrders: () => {
     const orders = localStorage.getItem("orders");
     return orders ? JSON.parse(orders) : [];
   },
-  
+  //subscribes to cart changes
   subscribe: (callback) => {
     subscribers.push(callback);
     return () => {
       subscribers = subscribers.filter(subscriber => subscriber !== callback);
     };
   },
-  
+  //notifies subscribers of cart changes
   notifySubscribers: () => {
     const cart = CartService.getCart();
     subscribers.forEach((subscriber) => subscriber(cart));
